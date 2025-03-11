462e78a875545552b596d187138c2b8988715b34
Ron <johnnyroy2718@gmail.com>
2025-03-11 08:44:03 -0700

    Toon shading

--- .godot/mono/temp/obj/Debug/MultiplayerFPSTutorial.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/MultiplayerFPSTutorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiplayerFPSTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce05d33218e253e56388f8da80bcd9bd0983c340")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7cac75f88626a5b51c738c69bd3c6b451ee098")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiplayerFPSTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiplayerFPSTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
